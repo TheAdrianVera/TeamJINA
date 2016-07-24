5eeb464982faf113e26501a1fff6416388cfa7a1
nadinelyab <nadinelyab@gmail.com>
2016-07-23 17:40:09 -0700

    adding layout and bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160723221505) do
 
   create_table "checklists", force: :cascade do |t|
